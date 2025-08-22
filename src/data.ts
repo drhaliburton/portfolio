97d1f2d17a4c285036496e4c359c57ba313da730
D Rebecca Haliburton <haliburtonre@gmail.com>
2025-08-22 11:57:23 -0300

    reduce duplication

--- src/data.ts
+++ src/data.ts
@@ -118,7 +118,7 @@ const { sidebar, summary, stack, education, experience }: Resume = {
       org: 'Regrow Ag',
       url: 'https://regrow.ag',
       subtitle: "March 2023 - Present",
-content: "Regrow is an agriculture resilience platform, where farmers can manage their practices and generate carbon credits from satellite monitored results. I was responsible for replatforming the MRV (measure, report, verify) experience, rewriting the front-end codebase, implementing a GraphQL backend-for-frontend, and refactoring the backend endpoints to support a new performant user experience. In tandem, I maintained the legacy application, implementing full-slice features in the Typescript, React, GraphQL, Node.js, Python and Go services.",    },
+content: "Regrow is an agriculture resilience platform, where farmers can manage their practices and generate carbon credits from satellite monitored results. I was responsible for rebilding the MRV (measure, report, verify) experience; rewriting the front-end codebase, implementing a GraphQL backend-for-frontend, and refactoring the backend endpoints to support a new performant user experience. In tandem, I maintained the legacy application, building full-slice features in the Typescript, React, GraphQL, Node.js, Python and Go services.",    },
     {
       title: "Full-Stack Software Engineer",
       org: "Bench Accounting",
